c4a12e8beed58647f5a1cfb43a91fff664bd554b
Raúl Piaggio <rpiaggio@gemini.edu>
2022-03-01 19:56:49 -0300

    Fix emission line decoder. Test. Organize imports.

--- lucuma-schemas/src/test/scala/lucuma/schemas/decoders/DecodersSuite.scala
+++ lucuma-schemas/src/test/scala/lucuma/schemas/decoders/DecodersSuite.scala
@@ -3,37 +3,42 @@
 
 package lucuma.schemas.decoders
 
-import cats.syntax.all._
+import cats.Order._
 import cats.effect._
+import cats.syntax.all._
 import coulomb._
+import eu.timepit.refined.types.numeric.PosBigDecimal
+import eu.timepit.refined.types.numeric.PosLong
+import eu.timepit.refined.types.string.NonEmptyString
+import io.circe.parser._
+import lucuma.core.enum.Band
+import lucuma.core.enum.CatalogName
+import lucuma.core.enum.GalaxySpectrum
+import lucuma.core.math.BrightnessUnits._
+import lucuma.core.math.BrightnessValue
+import lucuma.core.math.Coordinates
+import lucuma.core.math.Declination
+import lucuma.core.math.Epoch
+import lucuma.core.math.Parallax
+import lucuma.core.math.ProperMotion
+import lucuma.core.math.RadialVelocity
+import lucuma.core.math.RightAscension
+import lucuma.core.math.Wavelength
+import lucuma.core.math.dimensional._
+import lucuma.core.math.units._
+import lucuma.core.model.CatalogInfo
+import lucuma.core.model.EmissionLine
+import lucuma.core.model.SiderealTracking
+import lucuma.core.model.SourceProfile
+import lucuma.core.model.SpectralDefinition
+import lucuma.core.model.Target
+import lucuma.core.model.UnnormalizedSED
 import munit._
+
 import java.io.File
 import java.io.FileInputStream
 import java.nio.file.Paths
-import lucuma.core.model.SiderealTracking
-import lucuma.core.math.BrightnessUnits._
-import lucuma.core.math.Coordinates
-import lucuma.core.math.RightAscension
-import lucuma.core.math.Declination
-import lucuma.core.math.Epoch
-import lucuma.core.math.dimensional._
-import io.circe.parser._
-import lucuma.core.math.ProperMotion
-import lucuma.core.math.Parallax
-import lucuma.core.math.RadialVelocity
-import lucuma.core.model.Target
-import eu.timepit.refined.types.string.NonEmptyString
-import lucuma.core.model.SourceProfile
-import lucuma.core.model.SpectralDefinition
-import lucuma.core.model.UnnormalizedSED
-import lucuma.core.enum.GalaxySpectrum
-import lucuma.core.enum.Band
 import scala.collection.immutable.SortedMap
-import lucuma.core.math.units._
-import lucuma.core.math.BrightnessValue
-import eu.timepit.refined.types.numeric.PosLong
-import lucuma.core.model.CatalogInfo
-import lucuma.core.enum.CatalogName
 
 class DecodersSuite extends CatsEffectSuite {
 
@@ -44,7 +49,7 @@ class DecodersSuite extends CatsEffectSuite {
       IO.blocking(inStream.close()).handleErrorWith(_ => IO.unit) // release
     }
 
-  test("Target decoder - Point") {
+  test("Target decoder - Point - BandNormalized") {
     val expectedId     = Target.Id(PosLong(2))
     val expectedTarget =
       Target.Sidereal(
@@ -127,7 +132,58 @@ class DecodersSuite extends CatsEffectSuite {
     }
   }
 
-  test("Target decoder - Uniform") {
+  test("Target decoder - Point - EmissionLines") {
+    val expectedId     = Target.Id(PosLong(3))
+    val expectedTarget =
+      Target.Sidereal(
+        NonEmptyString("NGC 5949"),
+        SiderealTracking(
+          Coordinates(RightAscension.fromStringHMS.getOption("15:28:00.668").get,
+                      Declination.fromStringSignedDMS.getOption("+64:45:47.4").get
+          ),
+          Epoch.J2000,
+          ProperMotion.Zero.some,
+          RadialVelocity.fromMetersPerSecond.getOption(BigDecimal(423607)),
+          Parallax.Zero.some
+        ),
+        SourceProfile.Point(
+          SpectralDefinition.EmissionLines(
+            SortedMap(
+              Wavelength.unsafeFromInt(1000000)    -> EmissionLine(
+                PosBigDecimal(BigDecimal(1)).withUnit[KilometersPerSecond],
+                PosBigDecimal(BigDecimal(1)).withUnit[WattsPerMeter2].toMeasureTagged
+              ),
+              Wavelength.unsafeFromInt(1000000000) -> EmissionLine(
+                PosBigDecimal(BigDecimal(1)).withUnit[KilometersPerSecond],
+                PosBigDecimal(BigDecimal(1)).withUnit[ErgsPerSecondCentimeter2].toMeasureTagged
+              )
+            ),
+            PosBigDecimal(BigDecimal(12))
+              .withUnit[ErgsPerSecondCentimeter2Angstrom]
+              .toMeasureTagged
+          )
+        ),
+        none
+      )
+
+    val jsonFile = "/t3.json"
+    val url      = getClass().getResource(jsonFile)
+    val file     = Paths.get(url.toURI()).toFile()
+    inputStream(file).use { inStream =>
+      for {
+        str      <- IO.blocking(scala.io.Source.fromInputStream(inStream).mkString)
+        decoded   = for {
+                      json   <- parse(str)
+                      c       = json.hcursor.downField("data").downField("target")
+                      id     <- c.downField("id").as[Target.Id]
+                      target <- c.as[Target]
+                    } yield (id, target)
+        obtained <- IO.fromEither(decoded)
+      } yield assertEquals(obtained, (expectedId, expectedTarget))
+    }
+  }
+
+  test("Target decoder - Uniform - BandNormalized") {
     val expectedId     = Target.Id(PosLong(4))
     val expectedTarget =
       Target.Sidereal(
