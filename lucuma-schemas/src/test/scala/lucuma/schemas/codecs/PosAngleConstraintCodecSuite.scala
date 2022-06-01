91f9b4f8a2473df1c68766681c98be523a89810e
Shane Walker <swalker@gemini.edu>
2022-06-01 17:52:41 -0400

    Move codec tests

--- lucuma-schemas/src/test/scala/lucuma/schemas/codecs/PosAngleConstraintCodecSuite.scala
+++ lucuma-schemas/src/test/scala/lucuma/schemas/codecs/PosAngleConstraintCodecSuite.scala
@@ -1,17 +1,17 @@
 // Copyright (c) 2016-2022 Association of Universities for Research in Astronomy, Inc. (AURA)
 // For license information see LICENSE or https://opensource.org/licenses/BSD-3-Clause
 
-package lucuma.schemas.encoders
+package lucuma.schemas.codecs
 
 import io.circe.testing.CodecTests
 import io.circe.testing.instances.arbitraryJson
 import lucuma.core.model.arb.ArbPosAngle
 import lucuma.core.model.{ PosAngle => PosAngleConstraint }
 import lucuma.schemas.decoders._
-
+import lucuma.schemas.encoders._
 import munit.DisciplineSuite
 
-class PosAngleEncodersDisciplineSuite extends DisciplineSuite {
+class PosAngleConstraintCodecSuite extends DisciplineSuite {
 
   import ArbPosAngle._
 
