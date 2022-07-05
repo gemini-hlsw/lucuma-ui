f4fc6525b31cf011cf84ca441193c59e2122c890
Carlos Quiroz <carlos.quiroz@noirlab.edu>
2022-07-05 13:02:07 -0400

    Port ExposureTimeMode

--- lucuma-schemas/src/test/scala/lucuma/schemas/decoders/ExposureTimeModeDecodersSuite.scala
+++ lucuma-schemas/src/test/scala/lucuma/schemas/decoders/ExposureTimeModeDecodersSuite.scala
@@ -3,21 +3,26 @@
 
 package lucuma.schemas.decoders
 
-import eu.timepit.refined.auto._
+import eu.timepit.refined._
+import eu.timepit.refined.numeric.Positive
 import lucuma.core.model.ExposureTimeMode
 import lucuma.core.model.NonNegDuration
+import lucuma.refined._
 
 import java.time.Duration
 
 class ExposureTimeModeDecodersSuite extends InputStreamSuite {
   test("SignalToNoise decoder") {
-    val expected: ExposureTimeMode = ExposureTimeMode.SignalToNoise(value = BigDecimal("1.23"))
+    val expected: ExposureTimeMode =
+      ExposureTimeMode.SignalToNoise(value = refineV[Positive](BigDecimal(1.23)).getOrElse(sys.error("Cannot happen")))
     assertParsedStreamEquals("/signalToNoise.json", expected)
   }
 
   test("FixedExposure decoder") {
     val expected: ExposureTimeMode =
-      ExposureTimeMode.FixedExposure(count = 99, NonNegDuration.unsafeFrom(Duration.ofNanos(47000)))
+      ExposureTimeMode.FixedExposure(count = 99.refined,
+                                     NonNegDuration.unsafeFrom(Duration.ofNanos(47000))
+      )
     assertParsedStreamEquals("/fixedExposure.json", expected)
   }
 }
