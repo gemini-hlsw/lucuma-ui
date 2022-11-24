aea72e4ae6651b405d15b4d34d9595d2f190a288
Todd Burnside <tburnside@gemini.edu>
2022-11-24 14:14:15 -0300

    Add RadioButtonView and BooleanRadioButtons

---
+++ modules/ui/src/main/scala/lucuma/ui/primereact/BooleanRadioButtons.scala
@@ -0,0 +1,54 @@
+// Copyright (c) 2016-2022 Association of Universities for Research in Astronomy, Inc. (AURA)
+// For license information see LICENSE or https://opensource.org/licenses/BSD-3-Clause
+
+package lucuma.ui.primereact
+
+import eu.timepit.refined.types.string.NonEmptyString
+import japgolly.scalajs.react.*
+import japgolly.scalajs.react.vdom.html_<^._
+import react.common.*
+
+import scalajs.js
+
+final case class BooleanRadioButtons[V[_]: ViewLike] private (
+  trueButton:  RadioButtonView[V, Boolean],
+  falseButton: RadioButtonView[V, Boolean]
+):
+  def toTrueFalseFragment: TagMod = React.Fragment(trueButton, falseButton)
+  def toFalseTrueFragment: TagMod = React.Fragment(falseButton, trueButton)
+
+object BooleanRadioButtons {
+  def apply[V[_]: ViewLike](
+    idBase:     NonEmptyString, // base id for the 2 radio buttons. `-true` or `-false` is appended to this.
+    view:       V[Boolean],
+    name:       NonEmptyString, // name of "radio button group"
+    trueLabel:  NonEmptyString,
+    falseLabel: NonEmptyString,
+    trueClass:  js.UndefOr[Css] = js.undefined,
+    falseClass: js.UndefOr[Css] = js.undefined,
+    disabled:   js.UndefOr[Boolean] = js.undefined,
+    required:   js.UndefOr[Boolean] = js.undefined
+  ): BooleanRadioButtons[V] =
+    BooleanRadioButtons(
+      trueButton = RadioButtonView(
+        id = NonEmptyString.unsafeFrom(idBase.value + "-true"),
+        value = true,
+        view = view,
+        label = trueLabel,
+        name = name,
+        clazz = trueClass,
+        disabled = disabled,
+        required = required
+      ),
+      falseButton = RadioButtonView(
+        id = NonEmptyString.unsafeFrom(idBase.value + "-false"),
+        value = false,
+        view = view,
+        label = falseLabel,
+        name = name,
+        clazz = falseClass,
+        disabled = disabled,
+        required = required
+      )
+    )
+}
---
+++ modules/ui/src/main/scala/lucuma/ui/primereact/RadioButtonView.scala
@@ -0,0 +1,62 @@
+// Copyright (c) 2016-2022 Association of Universities for Research in Astronomy, Inc. (AURA)
+// For license information see LICENSE or https://opensource.org/licenses/BSD-3-Clause
+
+package lucuma.ui.primereact
+
+import cats.Eq
+import cats.syntax.all._
+import eu.timepit.refined.types.string.NonEmptyString
+import japgolly.scalajs.react.*
+import japgolly.scalajs.react.vdom.html_<^._
+import react.common.*
+import react.primereact.RadioButton
+
+import scalajs.js
+
+final case class RadioButtonView[V[_], A](
+  id:       NonEmptyString,
+  value:    A,                                         // The value this radio button will assign to the ExternalValue
+  view:     V[A],
+  label:    NonEmptyString,
+  name:     js.UndefOr[NonEmptyString] = js.undefined, // name of the "radio button group"
+  inputId:  js.UndefOr[String] = js.undefined,
+  disabled: js.UndefOr[Boolean] = js.undefined,
+  clazz:    js.UndefOr[Css] = js.undefined,
+  required: js.UndefOr[Boolean] = js.undefined,
+  onChange: js.UndefOr[(A, Boolean) => Callback] =
+    js.undefined, // shouldn't usually need to use this, the ViewLike is set internally.
+  modifiers:     Seq[TagMod] = Seq.empty
+)(using val eqA: Eq[A], val ev: ViewLike[V])
+    extends ReactFnProps(RadioButtonView.component):
+  def addModifiers(modifiers: Seq[TagMod]) = copy(modifiers = this.modifiers ++ modifiers)
+  def withMods(mods:          TagMod*)     = addModifiers(mods)
+  def apply(mods:             TagMod*)     = addModifiers(mods)
+
+object RadioButtonView {
+  type AnyF[_] = Any
+
+  private def buildComponent[V[_], A] = ScalaFnComponent[RadioButtonView[V, A]] { props =>
+    import props.given
+
+    <.div(
+      LucumaStyles.RadioButtonWithLabel,
+      RadioButton(
+        id = props.id.value,
+        value = props.value,
+        inputId = props.inputId,
+        name = props.name.map(_.value),
+        disabled = props.disabled,
+        clazz = props.clazz,
+        required = props.required,
+        modifiers = props.modifiers,
+        checked = props.view.get.exists(_ === props.value),
+        onChange = (a, checked) =>
+          props.view.set(a).when_(checked)
+            >> props.onChange.toOption.foldMap(_(a, checked))
+      ),
+      <.label(^.htmlFor := props.id.value, props.label.value)
+    )
+  }
+
+  private val component = buildComponent[AnyF, Any]
+}
