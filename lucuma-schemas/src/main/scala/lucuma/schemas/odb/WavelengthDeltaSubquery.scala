ac5d6be157a54d73f1f0ea5c69f892441e6f7681
Raúl Piaggio <rpiaggio@gemini.edu>
2023-02-15 10:18:58 -0300

    missing package

--- lucuma-schemas/src/main/scala/lucuma/schemas/odb/WavelengthDeltaSubquery.scala
+++ lucuma-schemas/src/main/scala/lucuma/schemas/odb/WavelengthDeltaSubquery.scala
@@ -1,6 +1,8 @@
 // Copyright (c) 2016-2023 Association of Universities for Research in Astronomy, Inc. (AURA)
 // For license information see LICENSE or https://opensource.org/licenses/BSD-3-Clause
 
+package lucuma.schemas.odb
+
 import clue.GraphQLSubquery
 import lucuma.core.math.WavelengthDelta
 import lucuma.schemas.ObservationDB
