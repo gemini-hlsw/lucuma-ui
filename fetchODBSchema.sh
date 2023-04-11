ebdc650ae69b866ed874ef1352d178fcaa23e0cb
Shane Walker <pistes.clary_0w@icloud.com>
2023-04-11 15:14:45 -0400

    fix test case + fetch script

--- fetchODBSchema.sh
+++ fetchODBSchema.sh
@@ -1,5 +1,41 @@
 #!/bin/bash
 
+KEY_MESSAGE=$(cat << END
+Define the ODB_API_KEY environment variable to use this script. For example:
+
+  export ODB_API_KEY="111.4ed718065f21a8f01014c8d08db2c0f74fe3e860ae25bb1f03c5ae4c5e2d0a0e677e6e781a086d0a6638960506cff7db"
+
+You can get an API key from the Explore "User Preferences" menu item.
+
+END
+)
+
+RED='\033[0;31m'
+NC='\033[0m'
+
+if [ -z "${ODB_API_KEY}" ]; then
+  echo -e "${RED}$KEY_MESSAGE${NC}"
+  exit 1
+fi
+
+function usage {
+  echo -e "${RED}Usage: $0 [local|staging]${NC}"
+  exit 1
+}
+
+case "$1" in
+  local)
+    URL="http://localhost:8082/odb"
+    ;;
+  staging)
+    URL="http://lucuma-postgres-odb-staging.herokuapp.com/odb"
+    ;;
+  *)
+    usage
+    ;;
+esac
+
+
 # gq https://lucuma-odb-development.herokuapp.com/odb --introspect >templates/src/main/resources/lucuma/schemas/ObservationDB.graphql
 # gq https://lucuma-odb-master.herokuapp.com/odb --introspect >templates/src/main/resources/lucuma/schemas/ObservationDB.graphql
-gq http://lucuma-postgres-odb-staging.herokuapp.com/odb -H 'Authorization: Bearer 111.4ed718065f21a8f01014c8d08db2c0f74fe3e860ae25bb1f03c5ae4c5e2d0a0e677e6e781a086d0a6638960506cff7db' --introspect >templates/src/main/resources/lucuma/schemas/ObservationDB.graphql
+gq $URL -H "Authorization: Bearer ${ODB_API_KEY}" --introspect >lucuma-schemas/src/clue/resources/lucuma/schemas/ObservationDB.graphql
--- lucuma-schemas/src/test/scala/lucuma/schemas/decoders/ExposureTimeModeDecodersSuite.scala
+++ lucuma-schemas/src/test/scala/lucuma/schemas/decoders/ExposureTimeModeDecodersSuite.scala
@@ -5,6 +5,7 @@ package lucuma.schemas.decoders
 
 import eu.timepit.refined._
 import eu.timepit.refined.numeric.Positive
+import lucuma.core.math.SignalToNoise
 import lucuma.core.model.ExposureTimeMode
 import lucuma.core.model.NonNegDuration
 import lucuma.core.syntax.timespan.*
@@ -13,15 +14,15 @@ import lucuma.refined._
 class ExposureTimeModeDecodersSuite extends InputStreamSuite {
   test("SignalToNoise decoder") {
     val expected: ExposureTimeMode =
-      ExposureTimeMode.SignalToNoise(value =
-        refineV[Positive](BigDecimal(1.23)).getOrElse(sys.error("Cannot happen"))
+      ExposureTimeMode.SignalToNoiseMode(value =
+        SignalToNoise.unsafeFromBigDecimalExact(BigDecimal(1.23))
       )
     assertParsedStreamEquals("/signalToNoise.json", expected)
   }
 
   test("FixedExposure decoder") {
     val expected: ExposureTimeMode =
-      ExposureTimeMode.FixedExposure(count = 99.refined, 47.µsTimeSpan)
+      ExposureTimeMode.FixedExposureMode(count = 99.refined, 47.µsTimeSpan)
     assertParsedStreamEquals("/fixedExposure.json", expected)
   }
 }
