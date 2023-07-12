cb8b2f37aace4d6bb8f674d138c6df888bdb5d90
Carlos Quiroz <carlos.quiroz@noirlab.edu>
2023-07-12 12:03:45 -0400

    Implement PR suggestions

--- modules/ui/src/main/scala/lucuma/ui/components/MoonPhase.scala
+++ modules/ui/src/main/scala/lucuma/ui/components/MoonPhase.scala
@@ -9,6 +9,14 @@ import lucuma.ui.syntax.all.given
 import react.common.ReactFnPropsWithChildren
 import react.common.style.Css
 
+object MoonStyles {
+  val MoonSphere: Css  = Css("moon-sphere")
+  val MoonLight: Css   = Css("moon-light")
+  val MoonDark: Css    = Css("moon-dark")
+  val MoonDivider: Css = Css("moon-divider")
+  val MoonPhase: Css   = Css("moon-phase")
+}
+
 case class MoonPhase(moonPhase: Double, clazz: Css = Css.Empty)
     extends ReactFnPropsWithChildren[MoonPhase](MoonPhase.component)
 
@@ -16,14 +24,14 @@ object MoonPhase {
   private type Props = MoonPhase
 
   private val component = ScalaFnComponent.withChildren[Props]((p, c) =>
-    <.div(GppStyles.MoonPhase)(
+    <.div(MoonStyles.MoonPhase)(
       // Adapted from https://dev.to/thormeier/use-your-i-moon-gination-lets-build-a-moon-phase-visualizer-with-css-and-js-aih
-      <.div(GppStyles.MoonSphere)(
-        <.div(GppStyles.MoonDark).when(p.moonPhase < 0.5),
-        <.div(GppStyles.MoonLight).when(p.moonPhase > 0.5),
-        <.div(GppStyles.MoonDark).when(p.moonPhase > 0.5),
-        <.div(GppStyles.MoonLight).when(p.moonPhase < 0.5),
-        <.div(GppStyles.MoonDivider, ^.transform := s"rotate3d(0, 1, 0, ${360 * p.moonPhase}deg")
+      <.div(MoonStyles.MoonSphere)(
+        <.div(MoonStyles.MoonDark).when(p.moonPhase < 0.5),
+        <.div(MoonStyles.MoonLight).when(p.moonPhase > 0.5),
+        <.div(MoonStyles.MoonDark).when(p.moonPhase > 0.5),
+        <.div(MoonStyles.MoonLight).when(p.moonPhase < 0.5),
+        <.div(MoonStyles.MoonDivider, ^.transform := s"rotate3d(0, 1, 0, ${360 * p.moonPhase}deg")
       ),
       c
     )
