60aa6bf3627956867fa9198627721512efb231bf
Raúl Piaggio <rpiaggio@gemini.edu>
2023-08-29 12:45:50 -0300

    fix typo

--- modules/ui/src/main/scala/lucuma/ui/sequence/SequenceStyles.scala
+++ modules/ui/src/main/scala/lucuma/ui/sequence/SequenceStyles.scala
@@ -6,13 +6,13 @@ package lucuma.ui.sequence
 import lucuma.react.common.Css
 
 object SequenceStyles:
-  val RightAligned: Css = Css("lucuma-sequece-right-aligned")
-  val LeftAligned: Css  = Css("lucuma-sequece-left-aligned")
-  val StepGuided: Css   = Css("lucuma-sequece-step-guided")
+  val RightAligned: Css = Css("lucuma-sequence-right-aligned")
+  val LeftAligned: Css  = Css("lucuma-sequence-left-aligned")
+  val StepGuided: Css   = Css("lucuma-sequence-step-guided")
 
   object StepType:
-    val Bias: Css   = Css("lucuma-sequece-step-type-bias")
-    val Dark: Css   = Css("lucuma-sequece-step-type-dark")
-    val Arc: Css    = Css("lucuma-sequece-step-type-arc")
-    val Flat: Css   = Css("lucuma-sequece-step-type-flat")
-    val Object: Css = Css("lucuma-sequece-step-type-object")
+    val Bias: Css   = Css("lucuma-sequence-step-type-bias")
+    val Dark: Css   = Css("lucuma-sequence-step-type-dark")
+    val Arc: Css    = Css("lucuma-sequence-step-type-arc")
+    val Flat: Css   = Css("lucuma-sequence-step-type-flat")
+    val Object: Css = Css("lucuma-sequence-step-type-object")
