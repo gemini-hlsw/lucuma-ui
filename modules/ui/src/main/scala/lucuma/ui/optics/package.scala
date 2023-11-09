b067fd4668f1bf59cd6161a4ae298369112f204e
Raúl Piaggio <rpiaggio@gemini.edu>
2023-11-08 21:21:20 -0300

    migrate optics from explore, disjointZip syntax

--- modules/ui/src/main/scala/lucuma/ui/optics/package.scala
+++ modules/ui/src/main/scala/lucuma/ui/optics/package.scala
@@ -4,44 +4,49 @@
 package lucuma.ui.optics
 
 import cats.syntax.all.*
+import eu.timepit.refined.types.numeric.NonNegInt
+import eu.timepit.refined.types.string.NonEmptyString
+import lucuma.core.optics.SplitEpi
+import lucuma.core.syntax.time.*
+import lucuma.core.util.TimeSpan
 import monocle.Iso
 import monocle.Lens
 
-// Lenses must be disjoint (not overlap), or the result will be unsafe.
-// See https://github.com/optics-dev/Monocle/issues/545
-def disjointZip[S, A, B](l1: Lens[S, A], l2: Lens[S, B]): Lens[S, (A, B)] =
-  Lens((s: S) => (l1.get(s), l2.get(s)))((ab: (A, B)) =>
-    (s: S) => l2.replace(ab._2)(l1.replace(ab._1)(s))
-  )
+import scala.collection.immutable.SortedSet
 
-def disjointZip[S, A, B, C](l1: Lens[S, A], l2: Lens[S, B], l3: Lens[S, C]): Lens[S, (A, B, C)] =
-  Lens((s: S) => (l1.get(s), l2.get(s), l3.get(s)))((abc: (A, B, C)) =>
-    (s: S) => l3.replace(abc._3)(l2.replace(abc._2)(l1.replace(abc._1)(s)))
-  )
+extension [S, A](l1: Lens[S, A])
+  // Lenses must be disjoint (not overlap), or the result will be unsafe.
+  // See https://github.com/optics-dev/Monocle/issues/545
+  def disjointZip[B](l2: Lens[S, B]): Lens[S, (A, B)] =
+    Lens((s: S) => (l1.get(s), l2.get(s)))((ab: (A, B)) =>
+      (s: S) => l2.replace(ab._2)(l1.replace(ab._1)(s))
+    )
 
-def disjointZip[S, A, B, C, D](
-  l1: Lens[S, A],
-  l2: Lens[S, B],
-  l3: Lens[S, C],
-  l4: Lens[S, D]
-): Lens[S, (A, B, C, D)] =
-  Lens((s: S) => (l1.get(s), l2.get(s), l3.get(s), l4.get(s)))((abc: (A, B, C, D)) =>
-    (s: S) => l4.replace(abc._4)(l3.replace(abc._3)(l2.replace(abc._2)(l1.replace(abc._1)(s))))
-  )
+extension [S, A, B](lenses: (Lens[S, A], Lens[S, B]))
+  def disjointZip: Lens[S, (A, B)] = lenses._1.disjointZip(lenses._2)
 
-def disjointZip[S, A, B, C, D, E](
-  l1: Lens[S, A],
-  l2: Lens[S, B],
-  l3: Lens[S, C],
-  l4: Lens[S, D],
-  l5: Lens[S, E]
-): Lens[S, (A, B, C, D, E)] =
-  Lens((s: S) => (l1.get(s), l2.get(s), l3.get(s), l4.get(s), l5.get(s)))((abc: (A, B, C, D, E)) =>
-    (s: S) =>
-      l5.replace(abc._5)(
-        l4.replace(abc._4)(l3.replace(abc._3)(l2.replace(abc._2)(l1.replace(abc._1)(s))))
-      )
-  )
+extension [S, A, B, C](lenses: (Lens[S, A], Lens[S, B], Lens[S, C]))
+  def disjointZip: Lens[S, (A, B, C)] =
+    ((lenses._1, lenses._2).disjointZip, lenses._3).disjointZip
+      .andThen(Iso[((A, B), C), (A, B, C)] { case ((a, b), c) => (a, b, c) } { case (a, b, c) =>
+        ((a, b), c)
+      })
+
+extension [S, A, B, C, D](lenses: (Lens[S, A], Lens[S, B], Lens[S, C], Lens[S, D]))
+  def disjointZip: Lens[S, (A, B, C, D)] =
+    ((lenses._1, lenses._2, lenses._3).disjointZip, lenses._4).disjointZip
+      .andThen(Iso[((A, B, C), D), (A, B, C, D)] { case ((a, b, c), d) => (a, b, c, d) } {
+        case (a, b, c, d) => ((a, b, c), d)
+      })
+
+extension [S, A, B, C, D, E](lenses: (Lens[S, A], Lens[S, B], Lens[S, C], Lens[S, D], Lens[S, E]))
+  def disjointZip: Lens[S, (A, B, C, D, E)] =
+    ((lenses._1, lenses._2, lenses._3, lenses._4).disjointZip, lenses._5).disjointZip
+      .andThen(Iso[((A, B, C, D), E), (A, B, C, D, E)] { case ((a, b, c, d), e) =>
+        (a, b, c, d, e)
+      } { case (a, b, c, d, e) =>
+        ((a, b, c, d), e)
+      })
 
 // This only behaves as a lawful lens as long as A and B are both null or both set.
 def unsafeDisjointOptionZip[S, A, B](
@@ -52,5 +57,18 @@ def unsafeDisjointOptionZip[S, A, B](
     (s: S) => l2.replace(ab.map(_._2))(l1.replace(ab.map(_._1))(s))
   )
 
-def optionIso[A, B](iso: Iso[A, B]): Iso[Option[A], Option[B]] =
-  Iso[Option[A], Option[B]](_.map(iso.get))(_.map(iso.reverseGet))
+extension [A, B](iso: Iso[A, B])
+  def option: Iso[Option[A], Option[B]] =
+    Iso[Option[A], Option[B]](_.map(iso.get))(_.map(iso.reverseGet))
+
+val OptionNonEmptyStringIso: Iso[Option[NonEmptyString], String] =
+  Iso[Option[NonEmptyString], String](_.foldMap(_.value))(s => NonEmptyString.from(s).toOption)
+
+// Note: truncates to Int.MaxValue - shouldn't have durations longer than that...
+val TimeSpanSecondsSplitEpi: SplitEpi[TimeSpan, NonNegInt] = SplitEpi(
+  ts => NonNegInt.unsafeFrom(math.min(ts.toSeconds.longValue, Int.MaxValue.toLong).toInt),
+  secs => TimeSpan.unsafeFromDuration(secs.value.toLong.seconds)
+)
+
+def SortedSetFromList[A: Ordering]: SplitEpi[List[A], SortedSet[A]] =
+  SplitEpi[List[A], SortedSet[A]](SortedSet.from(_), _.toList)
