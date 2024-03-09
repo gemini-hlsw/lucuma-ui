e3d102648ebd5a3784be47a7e45a87f8fae9b275
Carlos Quiroz <carlos.quiroz@noirlab.edu>
2024-03-09 13:47:50 -0300

    Component to copy to clipboard

---
+++ modules/ui/src/main/scala/lucuma/ui/components/CopyTextToClipboard.scala
@@ -0,0 +1,32 @@
+// Copyright (c) 2016-2023 Association of Universities for Research in Astronomy, Inc. (AURA)
+// For license information see LICENSE or https://opensource.org/licenses/BSD-3-Clause
+
+package lucuma.ui.components
+
+import japgolly.scalajs.react.*
+import japgolly.scalajs.react.vdom.html_<^.*
+import lucuma.react.common.*
+import org.scalajs.dom.window.navigator
+
+type OnCopy = (String, Boolean) => Callback
+
+/**
+ * Text to be copied to clipboard Optional callback, will be called when text is copied Optional
+ * copy-to-clipboard options.
+ */
+case class CopyTextToClipboard(
+  text:   String,
+  onCopy: OnCopy = (_, _) => Callback.empty
+) extends ReactFnPropsWithChildren[CopyTextToClipboard](CopyTextToClipboard.component)
+
+object CopyTextToClipboard:
+  private type Props = CopyTextToClipboard
+
+  private def copy(p: CopyTextToClipboard): Callback =
+    AsyncCallback.fromJsPromise(navigator.clipboard.writeText(p.text)).toCallback.attempt.flatMap {
+      case Right(_) => p.onCopy(p.text, true)
+      case Left(_)  => p.onCopy(p.text, false)
+    }
+
+  private val component = ScalaFnComponent
+    .withChildren[Props]((p, c) => <.div(^.onClick --> copy(p), c))
