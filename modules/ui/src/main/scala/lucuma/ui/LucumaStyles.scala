e924078f200509a0399085a1a972d7e686a25c38
Raúl Piaggio <rpiaggio@gemini.edu>
2024-03-20 08:24:41 -0300

    some more styles

---
+++ modules/ui/src/main/scala/lucuma/ui/LucumaStyles.scala
@@ -0,0 +1,11 @@
+// Copyright (c) 2016-2023 Association of Universities for Research in Astronomy, Inc. (AURA)
+// For license information see LICENSE or https://opensource.org/licenses/BSD-3-Clause
+
+package lucuma.ui
+
+import lucuma.react.common.Css
+
+object LucumaStyles:
+  val IndicatorOK: Css      = Css("lucuma-indicator-ok")
+  val IndicatorWarning: Css = Css("lucuma-indicator-warning")
+  val IndicatorFail: Css    = Css("lucuma-indicator-fail")
