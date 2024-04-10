3c8699cc882d7c8ffae96a3c53ccacf2185832c1
Hugo van Rijswijk <git@hugovr.nl>
2024-04-10 16:31:10 +0200

    Add scrollIfNeeded extension method

--- modules/ui/src/main/scala/lucuma/ui/syntax/util.scala
+++ modules/ui/src/main/scala/lucuma/ui/syntax/util.scala
@@ -3,8 +3,10 @@
 
 package lucuma.ui.syntax
 
+import japgolly.scalajs.react.Callback
 import japgolly.scalajs.react.Reusable
 import lucuma.react.common.EnumValue
+import org.scalajs.dom
 
 import scala.scalajs.js
 
@@ -15,5 +17,15 @@ trait util:
   extension [A](reusableList: Reusable[List[A]])
     def sequenceList: List[Reusable[A]] =
       reusableList.value.map(x => reusableList.map(_ => x))
+
+  extension (element: dom.Element)
+    /**
+     * Scroll the element into view if it is not fully visible.
+     */
+    def scrollIfNeeded: Callback = Callback.lift(() =>
+      val rect = element.getBoundingClientRect()
+      if (rect.top < 0) element.scrollIntoView()
+      if (rect.bottom > dom.window.innerHeight) element.scrollIntoView(false)
+    )
 
 object util extends util
