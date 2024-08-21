005eb98f0b97470fc7b1379107a8879bd7db26f9
Carlos Quiroz <carlos.quiroz@noirlab.edu>
2024-08-21 16:00:27 -0400

    Support dropdown templates

--- modules/ui/src/main/scala/lucuma/ui/primereact/EnumDropdownOptionalView.scala
+++ modules/ui/src/main/scala/lucuma/ui/primereact/EnumDropdownOptionalView.scala
@@ -16,24 +16,28 @@ import lucuma.react.primereact.SelectItem
 import scalajs.js
 
 final case class EnumDropdownOptionalView[V[_], A](
-  id:              NonEmptyString,
-  value:           V[Option[A]],
-  exclude:         Set[A] = Set.empty[A],
-  clazz:           js.UndefOr[Css] = js.undefined,
-  panelClass:      js.UndefOr[Css] = js.undefined,
-  showClear:       Boolean = true,
-  filter:          js.UndefOr[Boolean] = js.undefined,
-  showFilterClear: js.UndefOr[Boolean] = js.undefined,
-  disabled:        js.UndefOr[Boolean] = js.undefined,
-  placeholder:     js.UndefOr[String] = js.undefined,
-  size:            js.UndefOr[PlSize] = js.undefined,
-  onChangeE:       js.UndefOr[(Option[A], ReactEvent) => Callback] =
+  id:                   NonEmptyString,
+  value:                V[Option[A]],
+  exclude:              Set[A] = Set.empty[A],
+  clazz:                js.UndefOr[Css] = js.undefined,
+  panelClass:           js.UndefOr[Css] = js.undefined,
+  showClear:            Boolean = true,
+  filter:               js.UndefOr[Boolean] = js.undefined,
+  showFilterClear:      js.UndefOr[Boolean] = js.undefined,
+  disabled:             js.UndefOr[Boolean] = js.undefined,
+  placeholder:          js.UndefOr[String] = js.undefined,
+  size:                 js.UndefOr[PlSize] = js.undefined,
+  emptyMessage:         js.UndefOr[VdomNode] = js.undefined,
+  itemTemplate:         js.UndefOr[SelectItem[A] => VdomNode] = js.undefined,
+  valueTemplate:        js.UndefOr[SelectItem[A] => VdomNode] = js.undefined,
+  emptyMessageTemplate: js.UndefOr[VdomNode] = js.undefined,
+  onChangeE:            js.UndefOr[(Option[A], ReactEvent) => Callback] =
     js.undefined, // called after the view is set
-  modifiers:       Seq[TagMod] = Seq.empty
+  modifiers:            Seq[TagMod] = Seq.empty
 )(using
-  val enumerated:  Enumerated[A],
-  val display:     Display[A],
-  val vl:          ViewLike[V]
+  val enumerated:       Enumerated[A],
+  val display:          Display[A],
+  val vl:               ViewLike[V]
 ) extends ReactFnProps(EnumDropdownOptionalView.component):
   def addModifiers(modifiers: Seq[TagMod]) = copy(modifiers = this.modifiers ++ modifiers)
   def withMods(mods:          TagMod*)     = addModifiers(mods)
@@ -52,7 +56,7 @@ object EnumDropdownOptionalView {
       value = props.value.get.flatten,
       options = props.enumerated.all
         .filter(v => !props.exclude.contains(v))
-        .map(e => SelectItem(label = props.display.shortName(e), value = e)),
+        .map(e => SelectItem[A](label = props.display.shortName(e), value = e)),
       showClear = props.showClear,
       clazz = props.clazz.toOption.orEmpty |+| sizeCls,
       panelClass = props.panelClass.toOption.orEmpty |+| sizeCls,
@@ -60,6 +64,10 @@ object EnumDropdownOptionalView {
       showFilterClear = props.showFilterClear,
       placeholder = props.placeholder,
       disabled = props.disabled,
+      emptyMessage = props.emptyMessage,
+      itemTemplate = props.itemTemplate,
+      valueTemplate = props.valueTemplate,
+      emptyMessageTemplate = props.emptyMessageTemplate,
       onChange = v => props.value.set(v),
       onChangeE = props.onChangeE,
       modifiers = props.modifiers
