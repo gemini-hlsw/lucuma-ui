7b51c5b68c7ca81ee9ac3ba1c0d7fd44b87385e2
Raúl Piaggio <rpiaggio@gemini.edu>
2025-01-07 21:18:55 -0300

    reimplement toast.showDuring as Resource

--- modules/ui/src/main/scala/lucuma/ui/syntax/toast.scala
+++ modules/ui/src/main/scala/lucuma/ui/syntax/toast.scala
@@ -4,9 +4,10 @@
 package lucuma.ui.syntax
 
 import cats.Monoid
+import cats.effect.Resource
+import cats.effect.Resource.ExitCase
 import cats.effect.Sync
 import cats.effect.std.UUIDGen
-import cats.effect.syntax.all.*
 import cats.syntax.all.*
 import crystal.react.*
 import japgolly.scalajs.react.*
@@ -35,33 +36,47 @@ trait toast:
         )
       )
 
-    def showDuring[F[_]: Sync: UUIDGen](fa: F[Unit])(
+    def showDuring[F[_]: Sync: UUIDGen](
       text:         String,
       completeText: Option[String] = none,
       errorText:    Option[String] = none
-    )(using Monoid[F[Unit]]): F[Unit] =
+    )(using
+      Monoid[F[Unit]]
+    ): Resource[F, Unit] =
       for
-        id  <- UUIDGen[F].randomUUID
-        item = MessageItem(
-                 id = id.toString,
-                 content = <.span(
-                   LucumaIcons.CircleNotch.withSize(IconSize.LG).withFixedWidth(true),
-                   text
-                 ),
-                 severity = Message.Severity.Info,
-                 clazz = LucumaStyles.Toast,
-                 sticky = true,
-                 closable = false
-               )
-        _   <- toastRef.show(item).to[F]
-        _   <- fa.guarantee(toastRef.remove(item).to[F])
-                 .flatMap(_ => completeText.map(show(_).to[F]).orEmpty)
-                 .onError: e =>
-                   show(
-                     text = errorText.getOrElse(s"Error during operation: ${e.getMessage}"),
-                     severity = Message.Severity.Error,
-                     sticky = true
-                   ).to[F]
-      yield ()
+        item  <- Resource.eval:
+                   UUIDGen[F].randomUUID.map: id =>
+                     MessageItem(
+                       id = id.toString,
+                       content = <.span(
+                         LucumaIcons.CircleNotch.withSize(IconSize.LG).withFixedWidth(true),
+                         text
+                       ),
+                       severity = Message.Severity.Info,
+                       clazz = LucumaStyles.Toast,
+                       sticky = true,
+                       closable = false
+                     )
+        toast <- Resource.makeCase(
+                   toastRef.show(item).to[F]
+                 ): (_, exitCase) =>
+                   toastRef.remove(item).to[F] >> (
+                     exitCase match
+                       case ExitCase.Succeeded  =>
+                         completeText.map(show(_).to[F]).orEmpty
+                       case ExitCase.Errored(e) =>
+                         show(
+                           text = errorText.getOrElse(s"Error during operation: ${e.getMessage}"),
+                           severity = Message.Severity.Error,
+                           sticky = true
+                         ).to[F]
+                       case ExitCase.Canceled   =>
+                         show(
+                           text = errorText.getOrElse(s"Operation canceled"),
+                           severity = Message.Severity.Error,
+                           sticky = true
+                         ).to[F]
+                   )
+      yield toast
 
 object toast extends toast
