1e9eb8f97bf225158fd4e1b690cdeb5ed4a13f15
Raúl Piaggio <rpiaggio@gemini.edu>
2025-08-20 21:43:22 -0300

    migrate SequenceData from explore

---
+++ modules/ui/ui/src/main/scala/lucuma/ui/sequence/SequenceData.scala
@@ -0,0 +1,52 @@
+// Copyright (c) 2016-2025 Association of Universities for Research in Astronomy, Inc. (AURA)
+// For license information see LICENSE or https://opensource.org/licenses/BSD-3-Clause
+
+package lucuma.ui.sequence
+
+import cats.Eq
+import cats.derived.*
+import cats.syntax.all.*
+import japgolly.scalajs.react.ReactCats.*
+import japgolly.scalajs.react.Reusability
+import lucuma.core.enums.SequenceType
+import lucuma.core.model.sequence.InstrumentExecutionConfig
+import lucuma.itc.SingleSN
+import lucuma.itc.TotalSN
+import lucuma.schemas.odb.SequenceQueriesGQL.SequenceQuery
+import monocle.Focus
+import monocle.Lens
+
+/**
+ * Bundles the execution configuration and the signal-to-noise ratio for each sequence type.
+ */
+case class SequenceData(
+  config:     InstrumentExecutionConfig,
+  snPerClass: Map[SequenceType, (SingleSN, TotalSN)]
+) derives Eq
+
+object SequenceData:
+  private def buildSnMap(
+    itc: SequenceQuery.Data.Observation.Itc
+  ): Map[SequenceType, (SingleSN, TotalSN)] =
+    val acq: Option[(SequenceType, (SingleSN, TotalSN))] =
+      (itc.acquisition.selected.signalToNoiseAt.map(_.single),
+       itc.acquisition.selected.signalToNoiseAt.map(_.total)
+      ).mapN: (s, t) =>
+        SequenceType.Acquisition -> (SingleSN(s), TotalSN(t))
+
+    val sci: Option[(SequenceType, (SingleSN, TotalSN))] =
+      (itc.science.selected.signalToNoiseAt.map(_.single),
+       itc.science.selected.signalToNoiseAt.map(_.total)
+      ).mapN: (s, t) =>
+        SequenceType.Science -> (SingleSN(s), TotalSN(t))
+    List(acq, sci).flattenOption.toMap
+
+  def fromOdbResponse(data: SequenceQuery.Data): Option[SequenceData] =
+    (data.observation, data.executionConfig).mapN: (obs, config) =>
+      SequenceData(config, buildSnMap(obs.itc))
+
+  val config: Lens[SequenceData, InstrumentExecutionConfig]                  = Focus[SequenceData](_.config)
+  val snPerClass: Lens[SequenceData, Map[SequenceType, (SingleSN, TotalSN)]] =
+    Focus[SequenceData](_.snPerClass)
+
+  given Reusability[SequenceData] = Reusability.byEq
